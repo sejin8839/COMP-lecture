bc0076238241c3bbf2af4dedc0d00ceb9924b913
sejin8839 <sejinon01@gmail.com>
2023-03-14 17:41:59 +0900

    Update 0314.py

--- 0314.py
+++ 0314.py
@@ -1,23 +1,34 @@
 s='01234567'
 l=len(s)
-print(l)
-print(s[l-1])
-print(s[0:8:2])
-
-
-
-a=3
-b=0
-
+#print(l)
+#print(s[l-1])
+#print(s[0:8:2])
 
 
 if 20<10:
     print("20 is greater than 10")
-print('End...')
+#print('End...')
 
-weight = int(input("How many pounds does your suitcase weigh?"))
-if weight>50:
-    print("There is a $25 surcharge for heavy luggage")
-    print("Thank you")
+#weight = int(input("How many pounds does your suitcase weigh?"))
+#if weight>50:
+    #print("There is a $25 surcharge for heavy luggage")
+    #print("Thank you")
 
+
+
+x= int(input('enter x: '))
+y= int(input('enter x: '))
+if y !=0:
+    print(x/y)
+else:
+    print('Div by zero')
+
+
+
+
+number = int(input("Enter a number"))
+if number % 2 == 0:
+    print(f"{number} is Even number")
+else:
+    print(f"{number} is Odd number")
 
