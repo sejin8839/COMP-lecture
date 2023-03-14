19adaaa8e5cc0eeb916e133682e781abb84656cd
sejin8839 <sejinon01@gmail.com>
2023-03-14 17:28:52 +0900

    Update 0314.py

--- 0314.py
+++ 0314.py
@@ -3,3 +3,21 @@ l=len(s)
 print(l)
 print(s[l-1])
 print(s[0:8:2])
+
+
+
+a=3
+b=0
+
+
+
+if 20<10:
+    print("20 is greater than 10")
+print('End...')
+
+weight = int(input("How many pounds does your suitcase weigh?"))
+if weight>50:
+    print("There is a $25 surcharge for heavy luggage")
+    print("Thank you")
+
+
