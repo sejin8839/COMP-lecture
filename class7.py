9e1b61502b44e3ea73ea724ba40807845f723171
sejin8839 <sejinon01@gmail.com>
2023-03-16 17:13:36 +0900

    Create class7.py

---
+++ class7.py
@@ -0,0 +1,23 @@
+#Reverse
+S = "HELLO"
+reverse_name = ' '
+
+#1
+for c in S:
+    reverse_name = c + reverse_name
+
+print(f'name    : {S}')
+print(f'reverse : {reverse_name}')
+#2
+print(S[::-1])
+
+
+
+# f(x) = 1/x^2
+x= int(input('Enter x: '))
+if x>0:
+    print(1/(x^2))
+
+
+
+
