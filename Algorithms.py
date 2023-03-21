ce4e4afb84dc24192db71c38c4c409b073602979
sejin8839 <sejinon01@gmail.com>
2023-03-21 17:47:22 +0900

    Update Algorithms.py

--- Algorithms.py
+++ Algorithms.py
@@ -1,7 +1,11 @@
+
 n = int(input('Enter the value of n: '))
 sum = 0
 num=0
 while num< n:
     sum = sum + num
     num+=1
+    print(f"SUM = {sum}")
 print(f"SUM = {sum}")
+
+# ^ tab 하느냐 안 하느냐 차이 중요 하다는 거 확인 하기
