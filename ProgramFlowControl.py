297980de98dd54e10952d563bf91bef146a990ef
sejin8839 <sejinon01@gmail.com>
2023-03-21 17:26:17 +0900

    Update ProgramFlowControl.py

--- ProgramFlowControl.py
+++ ProgramFlowControl.py
@@ -1,13 +1,39 @@
 # conditionals
-if (True):
-    pass
-    print('hi')
-else:
-    pass
-    
+#if (True):
+    #pass
+    #print('hi')
+#else:
+    #pass
+
 # tab is 4 space bars.
 #can make multiple statements
 #why the : is important?
 
 
 #Loops
+s=0
+for x in range(1, 11):
+    s=s+x
+    print(s)
+
+s=0
+for x in range(1, 11):
+    if(x%2==0):
+        s_even=s+x
+print(s_even)
+
+# x%2==0 뜻은 x를 2로 나눴을 때 나머지가 0이면 true.
+
+
+# 각 글자 세로로 하나하나 출력
+s = 'Hello World'
+for l in s:
+    print(l)
+
+
+s = 'Hello World'
+i = 1
+while i<len(S):
+    print(s[i])
+    i=i+1
+    
