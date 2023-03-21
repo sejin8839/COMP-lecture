6eb1aa40de7c8134e381d4df85e4d964c080ff4e
sejin8839 <sejinon01@gmail.com>
2023-03-21 18:05:15 +0900

    Update Algorithms.py

--- Algorithms.py
+++ Algorithms.py
@@ -9,3 +9,26 @@ while num< n:
 print(f"SUM = {sum}")
 
 # ^ tab 하느냐 안 하느냐 차이 중요 하다는 거 확인 하기
+
+nums = input('Enter numbers, space separated:')
+sum1 = 0
+numbers = nums.split()
+for n in numbers:
+    sum1 = sum1 + int(n)
+print(f"SUM = {sum1}")
+
+
+
+rate = float(input("Enter rate: "))
+hours = float(input("Enter hours worked: "))
+if hours > 40:
+    pay = 40 * rate + (hours - 40) * 1.5 * rate
+else:
+    pay = hours * rate
+print(f"Pay=${pay}")
+
+
+
+
+
+
