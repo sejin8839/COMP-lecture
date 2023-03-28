bf8907d65f122df573ead17004bc87fcf1abae02
sejin8839 <sejinon01@gmail.com>
2023-03-28 17:26:18 +0900

    Update Turtle.py

--- Turtle.py
+++ Turtle.py
@@ -121,36 +121,114 @@
 
 
 # Turtle shape
-from turtle import *
-speed(10)
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
+# from turtle import *
+# speed(10)
+# color('red', 'yellow')
+# begin_fill()
+# while True:
+#     forward(200)
+#     left(170)
+#     if abs(pos()) < 1:
+#         break
+# end_fill()
+# done()
+
+
+color = ["red","purple","blue","green","orange","yellow"]
+colorIndex = 0
+
+import turtle
+window = turtle.Screen()
+turtle.bgcolor("black")
+Apple = turtle.Turtle()
+Apple.speed(10)
+Apple.shape("circle")
+Apple.color('red')
+Apple.penup()
+Apple.goto(50,50)
+Apple.pendown()
+appleWidth = 3
+dotSize = 2
+for x in range(1, 200, 3):
+    Apple.pencolor(color[colorIndex%6])
+    Apple.forward(x)
+    Apple.left(120)
+    Apple.dot(dotSize,"black")
+    Apple.right(60)
+    Apple.dot(dotSize,"black")
+    Apple.width(appleWidth)
+    colorIndex += 1
+
+    # It increases width AND dotSize in every loop
+    appleWidth += 0.1
+    dotSize += 2
+turtle.exitonclick()
+
+# import turtle
+# window = turtle.Screen()
+# turtle.bgcolor("black")
+# colors = [
+# "red","purple","blue","green","orange","yellow"]
+# Apple = turtle.Turtle()
+# Apple.speed(10)
+# Apple.shape("circle")
+# Apple.penup()
+# Apple.goto(50,50)
+# Apple.pendown()
+# appleWidth = 3
+# dotSize = 2
+#
+# for x in range(1,200,3):
+#
+#     # for y in range(360):
+#     #     Apple.color(colors[y % 6])
+#     Apple.forward(x)
+#     Apple.left(120)
+#     Apple.dot(dotSize,"black")
+#     Apple.right(60)
+#     Apple.dot(dotSize,"black")
+#     Apple.width(appleWidth)
+#
+#     # It increases width AND dotSize in every loop
+#     appleWidth += 0.1
+#     dotSize += 2
+
+
+turtle.exitonclick()
+
 
 
 # import turtle library
-import turtle
-colors = [
-"red","purple","blue","green","orange","yellow"]
-my_pen = turtle.Pen()
-turtle.bgcolor("black")
-for x in range(360):
-    my_pen.pencolor(colors[x % 6])
-    my_pen.width(x/100 + 1)
-    my_pen.forward(x)
-    my_pen.left(59)
-done()
+# # import turtle
+# colors = [
+# "red","purple","blue","green","orange","yellow"]
+# my_pen = turtle.Pen()
+# turtle.bgcolor("black")
+# for x in range(360):
+#     my_pen.pencolor(colors[x % 6])
+#     my_pen.width(x/100 + 1)
+#     my_pen.forward(x)
+#     my_pen.left(59)
+# done()
+#
+# # turtle demo examples
+# # Terminal 부분에 <python -m turtledemo> 입력하기
 
-# turtle demo examples
-# Terminal 부분에 <python -m turtledemo> 입력하기
 
 
+
+# import turtle
+# window = turtle.Screen()
+# spiral = turtle.Turtle()
+# spiral.color('green')
+# spiral.penup()
+# spiral.goto(0,-100)
+# spiral.pendown()
+# spiral.width(1)
+# for x in range(1,200,5):
+#     spiral.forward(x)
+#     spiral.left(90)
+# turtle.exitonclick()
 
 
 #turtle.showturtle()
