92ecfc7d0e1e783d2562b14608bc5d97c954c916
sejin8839 <sejinon01@gmail.com>
2023-03-28 17:49:34 +0900

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -6,11 +6,49 @@
 
 #random.seed(0)
 
+#
+# import random
+# random.seed(10)
+# lst = [1,2,3,4,5,6,7,8,9,10]
+# print(len(lst))
+# print(random.sample(lst,4))
+# print(random.sample(lst,2))
 
-import random
-random.seed(10)
-lst = [1,2,3,4,5,6,7,8,9,10]
-print(random.sample(lst,4))
+
+
+
+# import datetime
+#
+# print(datetime.datetime.now())
+# print(datetime.datetime.now().year)
+# print(datetime.datetime.today())
+#
+# #datetime type formatting
+# print(datetime.datetime(2019, 8, 24))
+# now=datetime.datetime.now()
+# current_time = now.strftime("%H:%M:%S")
+# print("Current time")
+
+
+
+# def hello():
+#     print("My first function")
+#
+# hello()
+
+
+
+# def add(a,b,c):
+#     print(f'sum = {a,b,c}')
+#     
+# add(10,10,10.5)
+# add(11,121,10)
+
+
+
+
+
+
 
 
 
