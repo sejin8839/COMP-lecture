d528c51a896f23272024385b375b1cc13a5fcf9a
sejin8839 <sejinon01@gmail.com>
2023-03-28 17:26:13 +0900

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,16 @@
+# import random
+# print(random.randon())
+# print(random.randrange(10))
+# print(random.randrange(0, 101, 2))
+# print(random.choice(['win', 'lose', 'draw']))
+
+#random.seed(0)
+
+
+import random
+random.seed(10)
+lst = [1,2,3,4,5,6,7,8,9,10]
+print(random.sample(lst,4))
+
+
+
