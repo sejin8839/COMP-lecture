0c12d3f2b1c2e34180b00b00e27f02e355eaafd0
sejin8839 <sejinon01@gmail.com>
2023-04-11 17:48:02 +0900

    Update BAEKJOON.py

--- BAEKJOON.py
+++ BAEKJOON.py
@@ -3,3 +3,19 @@ A = int(A)
 B = int(B)
 
 print(A-B)
+
+from Mymodule import *
+
+Sum=add(2, 6, 7)
+Product=multiply(2, 6, 7)
+print(f'total sum={Sum}')
+print{f'total product={Product}'}
+
+def add(*nums):
+    sum=0
+    for j in nums:
+        sum=sum+sum
+    return sum
+
+def multiply(*nums)
+
