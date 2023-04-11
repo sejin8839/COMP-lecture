ec6e93c7077a12f18882e4ede5bf5627868e245a
sejin8839 <sejinon01@gmail.com>
2023-04-11 17:48:07 +0900

    Create class11.py

---
+++ class11.py
@@ -0,0 +1,34 @@
+
+students = ['sejin', 'serum', 'hosung']
+print(students[0])
+
+
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+n = int(input("Enter a month number (1-12): "))
+print ("The month abbreviation is", months[n-1])
+
+
+myList = [1, "Spam", 4, "User"]
+print([1,2] + [3,4])
+[1, 2, 3, 4]
+print([1,2]*3)
+[1, 2, 1, 2, 1, 2]
+
+grades = ['A', 'B', 'C', 'D', 'F']
+print(grades[0])
+print(grades[2:4])
+print(len(grades))
+
+
+list_1 = [1, 3, 5, 7]
+list_2 = [2, 4, 6, 8]
+list_3 = [1, 3, 5, 7]
+list_4 = [7, 5, 3, 1]
+list_1 == list_2
+False
+list_1 == list_3
+True
+list_1 == list_4
+False
+
