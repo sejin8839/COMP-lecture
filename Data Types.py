1007187774b02e91573f0da8e8bd4d5468709207
sejin8839 <sejinon01@gmail.com>
2023-04-18 17:42:59 +0900

    Create Data Types.py

---
+++ Data Types.py
@@ -0,0 +1,42 @@
+fruits = ["grapefruit", "pineapple", "blueberries", "mango", "banana"]
+fruits[-2:-5]
+
+
+fruits[0]
+fruits [2]
+fruits [-3]
+fruits [9]
+
+# IndexError: list index out of range
+fruits[1:3]
+['pineapple', 'blueberries’]
+fruits[:3]
+['grapefruit', 'pineapple', 'blueberries’]
+fruits[2:]
+['blueberries', 'mango', 'banana’]
+fruits[1:4:2]
+['pineapple', 'mango’]
+fruits[-2:-5]
+['grapefruit', 'pineapple', 'blueberries', 'mango', 'banana’]
+fruits[::2]
+['grapefruit', 'blueberries', 'banana’]
+fruits[::-1]
+['banana', 'mango', 'blueberries', 'pineapple', 'grapefruit']
+
+
+
+Python Nested List
+nested_list = [[1] ,[17, 19], [21,23,25],
+[32,35,37,39]]
+>>> nested_list[2]
+[21, 23, 25]
+>>> nested_list[2][1]
+23
+>>> nested_list[2][1:3]
+[23, 25]
+>>> nested_list[1:4]
+[[17, 19], [21, 23, 25], [32, 35, 37, 39]]
+>>> nested_list[1:4][1:3]
+[[21, 23, 25], [32, 35, 37, 39]]
+>>> nested_list[1:4][-1][1:3]
+[35, 37]
