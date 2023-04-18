3d5ca0ec01b4f35f566cf32fed0cc7bbb0ff7296
sejin8839 <sejinon01@gmail.com>
2023-04-18 18:15:07 +0900

    Update Data Types.py

--- Data Types.py
+++ Data Types.py
@@ -28,27 +28,43 @@
 # Python Nested List
 # nested_list = [[1] ,[17, 19], [21,23,25],
 # [32,35,37,39]]
-# >>> nested_list[2]
-# [21, 23, 25]
-# >>> nested_list[2][1]
-# 23
-# >>> nested_list[2][1:3]
-# [23, 25]
-# >>> nested_list[1:4]
-# [[17, 19], [21, 23, 25], [32, 35, 37, 39]]
-# >>> nested_list[1:4][1:3]
-# [[21, 23, 25], [32, 35, 37, 39]]
-# >>> nested_list[1:4][-1][1:3]
-# [35, 37]
+# # >>> nested_list[2]
+# # [21, 23, 25]
+# # >>> nested_list[2][1]
+# # 23
+# # >>> nested_list[2][1:3]
+# # [23, 25]
+# # >>> nested_list[1:4]
+# # [[17, 19], [21, 23, 25], [32, 35, 37, 39]]
+# # >>> nested_list[1:4][1:3]
+# # [[21, 23, 25], [32, 35, 37, 39]]
+# # >>> nested_list[1:4][-1][1:3]
+# # [35, 37]
+#
+#
+# # A = [[1], [17,19], [21,23,25], [32,35[37,[39]]]]
+# # print(A[3][2])
+# # print(A[3][2][0])
+# # print(A[3][2][0])
+#
+#
+# numbers=[1,2,3,4,5]
+# print(sum(numbers))
+# print(max(numbers))
+# print(min(numbers))
+# x = any([1, 1, 0, 0, 1, 0])
+# print((x))
+#
+# print(all([1, 1, 1, 1])
+
+# names = ['Kim', ' lee', 'Dave', 'Bob']
+# print(sorted(names))
+# print(names)
 
 
-A = [[1], [17, 19], [21,23,25], [32,35[37,[39]]]]
-print(A[3][2])
-print(A[3][2][0])
-print(A[3][2][0])
-
-
-
+names = [10, 300, 200, 5]
+print(sorted(names,reverse=True))
+print(names)
 
 
 
