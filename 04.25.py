35dcf0453dc8343b6aab3ddd43d0257ff29f2611
sejin8839 <sejinon01@gmail.com>
2023-04-25 17:22:59 +0900

    Update 04.25.py

--- 04.25.py
+++ 04.25.py
@@ -1,6 +1,35 @@
+##
 basket={'apple','orange','apple','pear','orange','banana'}
 
+#1
 print(type(basket))
+#2
+print(basket)
+#3
+print(len(basket))
+#4
+print('orange'in basket)
+#5
+print('crabgrass'in basket)
+
+
+##
+a=set('abracadabra')
+b=set('alacazam')
+print(a)
+print(b)
+print(a-b)
+
+
+##
+A={}
+print(type(A))
+
+B=set()
+print(type(B))
+
+
+
 
 
 
